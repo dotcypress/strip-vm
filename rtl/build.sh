4708a44c17f489a4116ea35d471e119e51f756a5
Vitaly Domnikov <dotcypress@gmail.com>
2020-05-27 14:09:27 -0700

    update script

--- rtl/build.sh
+++ rtl/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cargo run --release -- compile ../docs/boot.s ../docs/boot.bin
-xxd -p -c 4 ../docs/boot.bin > boot.rom
+cargo run --release -- compile docs/boot.s docs/boot.bin
+xxd -p -c 4 docs/boot.bin > rtl/boot.rom
